4a010772b4f6c0fede9f62b27b874230f4feaeb6
evandor <evandor@gmail.com>
2015-02-03 18:09:12 +0100

    javadoc fix

--- skysail.server.um/src/io/skysail/server/um/security/shiro/util/RestletRequestPairSource.java
+++ skysail.server.um/src/io/skysail/server/um/security/shiro/util/RestletRequestPairSource.java
@@ -4,9 +4,10 @@ import org.restlet.Request;
 import org.restlet.Response;
 
 /**
- * A {@code RestletRequestPairSource} is a provider of a {@link Request Request} and
- * {@link Response Response} pair associated with a currently executing request.  This is 
- * a similar interface to {@link RequestPairResource} for restlet environments.
+ * A {@code RestletRequestPairSource} is a provider of a {@link Request Request}
+ * and {@link Response Response} pair associated with a currently executing
+ * request. This is a similar interface to @link RequestPairResource for restlet
+ * environments.
  *
  * @since 1.0
  */

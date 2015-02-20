29090b8bdd8441e634b2ba30f994c76c3415f01c
Carsten Gräf <evandor@gmail.com>
2015-02-20 07:16:45 +0100

    SSF-1: move to restlet 2.3.x
    Task-Url: http://jira.twentyeleven.de/browse/SSF-1

--- skysail.product.todos/deployment/scripts/deploy_prod.sh
+++ skysail.product.todos/deployment/scripts/deploy_prod.sh
@@ -2,42 +2,44 @@
 
 export JAVA_HOME=/home/carsten/.hudson/tools/hudson.model.JDK/java_SDK_8u25/
 
-#cd /home/carsten/.hudson/jobs/skysail.product.website.export.prod/workspace/skysail.products.website/generated/distributions/executable
+cd /home/carsten/.hudson/jobs/skysail.product.todos.export.prod/workspace/skysail.product.todos/generated/distributions/executable
 
 #echo "Creating ZIP Archive"
-#cp prod.jar skysail.website.jar
-#zip -r skysail.website.zip ../../../config/prod skysail.website.jar
+cp prod.jar skysail.todos.jar
+#zip -r skysail.todos.zip ../../../config/prod skysail.todos.jar
+zip -r skysail.todos.zip skysail.todos.jar
 
-##echo "copying skysail.website.zip to public site"
-##cp skysail.website.zip /var/www/skysail/products/website/skysail.website.zip
+echo "copying skysail.todos.zip to public site"
+cp skysail.todos.zip /var/www/skysail/products/todos/skysail.todos.zip
 
-#mkdir -p /home/carsten/skysail/products/website/prod/bin
-#mkdir -p /home/carsten/skysail/products/website/prod/lib
+mkdir -p /home/carsten/skysail/products/todos/prod/bin
+mkdir -p /home/carsten/skysail/products/todos/prod/lib
 
-#echo "copying skysail.website.jar to products directory"
-#cp skysail.website.jar /home/carsten/skysail/products/website/prod/bin/skysail.website.jar
+echo "copying skysail.todos.jar to products directory"
+cp skysail.todos.jar /home/carsten/skysail/products/todos/prod/bin/skysail.todos.jar
 
-#echo "stopping website service"
-#/home/carsten/skysail/products/website/prod/bin/website_prod stop
+echo "stopping todos service"
+/home/carsten/skysail/products/todos/prod/bin/todos_prod stop
 
-#cd /home/carsten/.hudson/jobs/skysail.product.website.export.prod/workspace/skysail.products.website
-#cp -r deployment/service/* /home/carsten/skysail/products/website/prod
+cd /home/carsten/.hudson/jobs/skysail.product.todos.export.prod/workspace/skysail.product.todos
+cp -r deployment/service/* /home/carsten/skysail/products/todos/prod
 
-## needed for website functionality (to get access to the contained jars for compiling)
-#unzip -o /home/carsten/skysail/products/website/prod/bin/skysail.website.jar
+# needed for todos functionality (to get access to the contained jars for compiling)
+#unzip -o /home/carsten/skysail/products/todos/prod/bin/skysail.todos.jar
 
 #echo "getting config files for installation from svn"
-#cd /home/carsten/skysail/products/website/prod/bin/config
+#cd /home/carsten/skysail/products/todos/prod/bin/config
 #rm -rf prod
-#svn checkout https://85.25.22.125/repos/skysale/skysailconfigs/website/prod/
+#svn checkout https://85.25.22.125/repos/skysale/skysailconfigs/todos/prod/
 
-#echo "starting website service"
-#cd /home/carsten/skysail/products/website/prod/bin/
+#echo "starting todos service"
+cd /home/carsten/skysail/products/todos/prod/bin/
 #rm -rf META-INF
 #rm -rf aQute
 #rm -rf jar
-#unzip -o skysail.website.jar
-#./website_prod start
+# not really necessary:
+unzip -o skysail.todos.jar
+./todos_prod start
 
 
 

8c7f213674cca3c165f9c8968351de9aef6abb12
Carsten Gräf <evandor@gmail.com>
2015-05-03 11:14:38 +0200

    addding runtime deployment for downloadable installation

---
+++ skysail.product.todos/deployment/scripts/deploy_runtime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+export JAVA_HOME=/home/carsten/.hudson/tools/hudson.model.JDK/java_SDK_8u25/
+
+cd /home/carsten/.hudson/jobs/skysail.product.todos.export.int/workspace/skysail.product.todos/generated/distributions/executable
+
+#echo "Creating ZIP Archive"
+cp todos.runtime.jar skysail.todos.runtime.jar
+
+zip -r skysail.todos.runtime.zip ../../../config/runtime skysail.todos.runtime.jar
+
+echo "copying skysail.todos.runtime.zip to public site"
+cp skysail.todos.runtime.zip /var/www/skysail/products/todos/skysail.todos.runtime.zip

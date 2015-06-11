957d04e3fff7b6621c51752ea38bba0330706889
evandor <evandor@gmail.com>
2015-06-11 16:33:30 +0200

    wip

--- skysail.server.ext.sse/src/io/skysail/server/ext/sse/resources/GuiMessageResource.java
+++ skysail.server.ext.sse/src/io/skysail/server/ext/sse/resources/GuiMessageResource.java
@@ -1,5 +1,6 @@
 package io.skysail.server.ext.sse.resources;
 
+import io.skysail.server.app.SkysailApplication;
 import io.skysail.server.ext.sse.Message;
 import io.skysail.server.ext.sse.SseApplication;
 
@@ -9,7 +10,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.shiro.SecurityUtils;
-import org.restlet.data.MediaType;
 import org.restlet.representation.Representation;
 import org.restlet.representation.WriterRepresentation;
 import org.restlet.resource.ResourceException;
@@ -50,49 +50,55 @@ public class GuiMessageResource extends ServerResource {
 
         System.out.println(sb.toString());
 
-//        StringRepresentation representation = new StringRepresentation(sb.toString());
-//        representation.setMediaType(SkysailApplication.SKYSAIL_SERVER_SENT_EVENTS);
-//        representation.setCharacterSet(CharacterSet.UTF_8);
+        // StringRepresentation representation = new
+        // StringRepresentation(sb.toString());
+        // representation.setMediaType(SkysailApplication.SKYSAIL_SERVER_SENT_EVENTS);
+        // representation.setCharacterSet(CharacterSet.UTF_8);
 
-//        PipedInputStream pi = new PipedInputStream();
-//        PipedOutputStream po;
-//        try {
-//            po = new PipedOutputStream(pi);
-//            Representation ir = new OutputRepresentation(SkysailApplication.SKYSAIL_SERVER_SENT_EVENTS) {
-//                @Override
-//                public void write(OutputStream realOutput) throws IOException {
-//                    byte[] b = new byte[8];
-//                    int read;
-//                    while ((read = pi.read(b)) != -1) {
-//                        realOutput.write(b, 0, read);
-//                        realOutput.flush();
-//                    }
-//                }
-//            };
-//            representation.setCharacterSet(CharacterSet.UTF_8);
-//            OutputStreamWriter ow = new OutputStreamWriter(po);
-//            PrintWriter out = new PrintWriter(ow, true);
-//            new Thread(new LongRunningBeast(out)).start();
-//            return ir;
-//        } catch (IOException e1) {
-//            e1.printStackTrace();
-//        }
+        // PipedInputStream pi = new PipedInputStream();
+        // PipedOutputStream po;
+        // try {
+        // po = new PipedOutputStream(pi);
+        // Representation ir = new
+        // OutputRepresentation(SkysailApplication.SKYSAIL_SERVER_SENT_EVENTS) {
+        // @Override
+        // public void write(OutputStream realOutput) throws IOException {
+        // byte[] b = new byte[8];
+        // int read;
+        // while ((read = pi.read(b)) != -1) {
+        // realOutput.write(b, 0, read);
+        // realOutput.flush();
+        // }
+        // }
+        // };
+        // representation.setCharacterSet(CharacterSet.UTF_8);
+        // OutputStreamWriter ow = new OutputStreamWriter(po);
+        // PrintWriter out = new PrintWriter(ow, true);
+        // new Thread(new LongRunningBeast(out)).start();
+        // return ir;
+        // } catch (IOException e1) {
+        // e1.printStackTrace();
+        // }
 
-        Representation representation = new WriterRepresentation(MediaType.TEXT_PLAIN)
-        {
+        Representation representation = new WriterRepresentation(SkysailApplication.SKYSAIL_SERVER_SENT_EVENTS) {
             @Override
-            public void write(Writer writer) throws IOException
-            {
+            public void write(Writer writer) throws IOException {
                 String json = "{\"foo\" : \"bar\"}";
-                while (true)
-                {
-                    writer.write(json);
+                while (true) {
+                    try {
+                        Thread.sleep(10000);
+                        writer.write("data: hier\n\n");
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                 }
             }
         };
 
         return representation;
-        //return new StringRepresentation ("error creating server sent events");
+        // return new StringRepresentation
+        // ("error creating server sent events");
     }
 
 }

def2f8b3897205ed0cf34d9f8d1a0339cb573c9b
Carsten Gräf <evandor@gmail.com>
2015-07-04 19:03:34 +0200

    new stuff from develop

---
+++ skysail.server/src/io/skysail/server/restlet/resources/WebComponentCall.java
@@ -0,0 +1,28 @@
+package io.skysail.server.restlet.resources;
+
+import lombok.*;
+
+@Getter
+@ToString
+@Builder
+public class WebComponentCall {
+
+    private final String type;
+    private String url;
+    private String linkTo;
+    private String title;
+    private String identifier;
+    private String requestUrl;
+
+    public String getHtml() {
+        return "<"+type+" url=\""+url+"\" link-to=\""+linkTo+"\" "+identifiersAttribute()+" request-url=\""+requestUrl+"\"></"+type+">";
+    }
+
+    private String identifiersAttribute() {
+        if (identifier == null) {
+            return "";
+        }
+        return "identifier=\""+identifier+"\"";
+    }
+   
+}

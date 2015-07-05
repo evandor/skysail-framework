20c926672ddf79d8de1323358d895b2c3fa4c3a8
Carsten Gräf <evandor@gmail.com>
2015-07-05 13:33:58 +0200

    wip

---
+++ skysail.product.todos/src/io/skysail/product/todos/Dependencies.java
@@ -0,0 +1,10 @@
+package io.skysail.product.todos;
+
+/**
+ * this is to make sure that the required modules are added to the exported jar.
+ */
+public class Dependencies {
+
+    private io.skysail.api.documentation.DocumentationProvider provider;
+    
+}
--- skysail.product.todos/src/io/skysail/product/todos/Dummy.java
+++
@@ -1,5 +0,0 @@
-package io.skysail.product.todos;
-
-public class Dummy {
-
-}

7a3217e6d029ac7162054a47c2fccbb8d28e0c93
Carsten Gräf <evandor@gmail.com>
2015-08-25 08:05:53 +0200

    some logging

--- skysail.server.um.simple/src/io/skysail/server/um/simple/authentication/SkysailCookieAuthenticator.java
+++ skysail.server.um.simple/src/io/skysail/server/um/simple/authentication/SkysailCookieAuthenticator.java
@@ -1,5 +1,7 @@
 package io.skysail.server.um.simple.authentication;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.cache.*;
 import org.apache.shiro.subject.Subject;
@@ -14,13 +16,14 @@ import de.twenty11.skysail.server.app.SkysailRootApplication;
 /**
  * Overwriting the restlet cookie authenticator to provide a more specific
  * implementation.
- * 
+ *
  * <p>
  * Optional is set to true to allow anonymous access, the credentials cookie
  * path is set to "/" and a verifier is set.
  * </p>
  *
  */
+@Slf4j
 public class SkysailCookieAuthenticator extends CookieAuthenticator {
 
     private CacheManager cacheManager;
@@ -50,10 +53,11 @@ public class SkysailCookieAuthenticator extends CookieAuthenticator {
         credentialsCookie.setPath("/");
         return credentialsCookie;
     }
-    
+
     @Override
     protected boolean authenticate(Request request, Response response) {
         // Restore credentials from the cookie
+        log.debug("getting cookie with name {}", getCookieName());
         Cookie credentialsCookie = request.getCookies().getFirst(
                 getCookieName());
 
@@ -61,6 +65,7 @@ public class SkysailCookieAuthenticator extends CookieAuthenticator {
             if (byPassIfPublicUrl(request)) {
                 return false;// super.authenticate(request, response);
             }
+            log.debug("providing cookie with value '{}'", credentialsCookie.getValue());
             request.setChallengeResponse(parseCredentials(credentialsCookie
                     .getValue()));
         }
@@ -79,7 +84,7 @@ public class SkysailCookieAuthenticator extends CookieAuthenticator {
         int result = super.logout(request, response);
         if (cacheManager != null) {
             Cache<Object, Object> cache = cacheManager.getCache(SkysailHashedCredentialsMatcher.CREDENTIALS_CACHE);
-            // need to find the current value: 
+            // need to find the current value:
             //cache.remove(value);
             // instead: remove all for now
             cache.clear();

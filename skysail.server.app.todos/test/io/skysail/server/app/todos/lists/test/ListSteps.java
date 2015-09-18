751660f8c71e4306b35c47daf13e5f8d901e4ad6
Carsten Gräf <evandor@gmail.com>
2015-09-18 07:22:52 +0200

    fix

--- skysail.server.app.todos/test/io/skysail/server/app/todos/lists/test/ListSteps.java
+++ skysail.server.app.todos/test/io/skysail/server/app/todos/lists/test/ListSteps.java
@@ -1,23 +1,21 @@
-//package io.skysail.server.app.todos.lists.test;
-//
-//import io.skysail.api.responses.SkysailResponse;
-//import io.skysail.server.app.todos.TodoList;
-//import net.thucydides.core.annotations.Step;
-//
-//import org.restlet.data.Form;
-//
-//public class ListSteps extends AbstractListResourceTest  {
-//
-//    @Step("Given a traveller has a frequent flyer account with {0} points")
-//    public void a_traveller_has_a_frequent_flyer_account_with_balance(int initialBalance) {
-//       // frequentFlyer.withInitialBalanceOf(initialBalance);
-//    }
-//
-//    @Step("xxx_posting")
-//    public void when_posting_this(String key, String val) {
-//        Form form = new Form();
-//        form.add(key, val);
-//        SkysailResponse<TodoList> result = postListresource.post(form, HTML_VARIANT);
-//        assertListResult(postListresource, result, "list1");
-//    }
-//}
+package io.skysail.server.app.todos.lists.test;
+
+import net.thucydides.core.annotations.Step;
+
+import org.restlet.data.Form;
+
+public class ListSteps {
+
+    @Step("Given a traveller has a frequent flyer account with {0} points")
+    public void a_traveller_has_a_frequent_flyer_account_with_balance(int initialBalance) {
+       // frequentFlyer.withInitialBalanceOf(initialBalance);
+    }
+
+    @Step("xxx_posting")
+    public void when_posting_this(String key, String val) {
+        Form form = new Form();
+        form.add(key, val);
+        //SkysailResponse<TodoList> result = postListresource.post(form, HTML_VARIANT);
+        //assertListResult(postListresource, result, "list1");
+    }
+}
--- skysail.server.app.todos/test/io/skysail/server/app/todos/lists/test/ListTests.java
+++ skysail.server.app.todos/test/io/skysail/server/app/todos/lists/test/ListTests.java
@@ -1,31 +1,31 @@
-//package io.skysail.server.app.todos.lists.test;
-//
-//import net.serenitybdd.junit.runners.SerenityRunner;
-//import net.thucydides.core.annotations.Steps;
-//
-//import org.junit.*;
-//import org.junit.runner.RunWith;
-//import org.mockito.MockitoAnnotations;
-//
-//@RunWith(SerenityRunner.class)
-//public class ListTests extends AbstractListResourceTest {
-//
-//    @Steps
-//    ListSteps listSteps;
-//
-//    @Before
-//    public void setUp() {
-//        MockitoAnnotations.initMocks(this);
-//    }
-//
-//    @Test
-//    public void shouldCalculatePointsBasedOnDistance() {
-//        listSteps.when_posting_this("hi","there");
-//
-//        //listSteps.the_traveller_flies(0);
-//
-//        //listSteps.traveller_should_have_a_balance_of(10000);
-//
-//    }
-//
-//}
+package io.skysail.server.app.todos.lists.test;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(SerenityRunner.class)
+public class ListTests extends AbstractListResourceTest {
+
+    @Steps
+    ListSteps listSteps;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldCalculatePointsBasedOnDistance() {
+        listSteps.when_posting_this("hi","there");
+
+        //listSteps.the_traveller_flies(0);
+
+        //listSteps.traveller_should_have_a_balance_of(10000);
+
+    }
+
+}

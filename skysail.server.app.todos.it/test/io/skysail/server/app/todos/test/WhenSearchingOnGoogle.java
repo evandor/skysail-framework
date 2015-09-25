c8352380509e83e4f91924bb023f1504c42dec9e
evandor <evandor@gmail.com>
2015-09-25 15:13:51 +0200

    wip

---
+++ skysail.server.app.todos.it/test/io/skysail/server/app/todos/test/WhenSearchingOnGoogle.java
@@ -0,0 +1,31 @@
+package io.skysail.server.app.todos.test;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@RunWith(SerenityRunner.class)
+public class WhenSearchingOnGoogle {
+
+    @Managed(driver = "htmlunit")
+    WebDriver driver;
+
+    @Test
+    public void shouldInstantiateAWebDriverInstanceForAWebTest() {
+
+        driver.get("http://www.google.com");
+
+        driver.findElement(By.name("q")).sendKeys("firefly", Keys.ENTER);
+
+       new WebDriverWait(driver,5).until(titleContains("Google Search"));
+
+        //assertThat(driver.getTitle()).isEqualTo("firefly - Google Search");
+    }
+
+
+}

cbdb6c211b149e6464c1b7041664e38648063399
Carsten Gräf <evandor@gmail.com>
2015-10-05 06:55:03 +0200

    trying to make build work

---
+++ skysail.product.todos/test/io/skysail/product/todos/test/DummyTest.java
@@ -0,0 +1,14 @@
+package io.skysail.product.todos.test;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SerenityRunner.class)
+public class DummyTest {
+
+    @Test
+    public void this_is_to_create_the_serenity_output_directory() {
+    }
+}

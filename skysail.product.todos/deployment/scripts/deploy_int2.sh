e469d986943864322b4a0328a06407e15f1ecd91
Carsten Gräf <evandor@gmail.com>
2015-10-05 18:02:13 +0200

    wip

--- skysail.product.todos/deployment/scripts/deploy_int2.sh
+++ skysail.product.todos/deployment/scripts/deploy_int2.sh
@@ -3,7 +3,7 @@
 export JAVA_HOME=/home/carsten/.hudson/tools/hudson.model.JDK/java_SDK_8u25/
 
 JOB_DIR="/home/carsten/.hudson/jobs/ssp.todos.export.int/workspace/skysail.product.todos"
-PRODUCT_DIR="/home/carsten/skysail/products/todos"
+PRODUCT_DIR="/home/carsten/skysail/products/todos/int2"
 
 cd $JOB_DIR/generated/distributions/executable
 
@@ -13,34 +13,34 @@ cp todos.int2.jar skysail.todos.jar
 echo "copying skysail.todos.jar to public site"
 cp skysail.todos.jar /var/www/skysail/products/todos/skysail.todos.int2.jar
 
-mkdir -p $PRODUCT_DIR/int2/bin/config/int
-mkdir -p $PRODUCT_DIR/int2/lib
+mkdir -p $PRODUCT_DIR/bin/config/int
+mkdir -p $PRODUCT_DIR/lib
 
 echo "copying skysail.todos.jar to products directory"
-cp skysail.todos.jar $PRODUCT_DIR/int2/bin/skysail.todos.jar
+cp skysail.todos.jar $PRODUCT_DIR/bin/skysail.todos.jar
 
-echo "stopping todos service: $PRODUCT_DIR/int2/bin/todos_int2"
-if [ -e "$PRODUCT_DIR/int2/bin/todos_int2" ]
+echo "stopping todos service: $PRODUCT_DIR/bin/todos_int2"
+if [ -e "$PRODUCT_DIR/bin/todos_int2" ]
 then
-  chmod 755 $PRODUCT_DIR/int2/bin/todos_int2
-  $PRODUCT_DIR/int2/bin/todos_int2 stop
+  chmod 755 $PRODUCT_DIR/bin/todos_int2
+  $PRODUCT_DIR/bin/todos_int2 stop
 fi
 
 
 cd $JOB_DIR
-cp -r deployment/service/* $PRODUCT_DIR/int2
-cp config/int/* $PRODUCT_DIR/int2/bin/config/int
+cp -r deployment/service/* $PRODUCT_DIR
+cp config/int/* $PRODUCT_DIR/bin/config/int
 
 
 echo "getting config files for installation from svn"
-cd $PRODUCT_DIR/int2/bin
+cd $PRODUCT_DIR/bin
 #rm -rf config
 #mkdir config
 cd config
 svn export --force https://85.25.22.125/repos/skysale/skysailconfigs/todos/int/
 
 echo "starting todos service"
-cd $PRODUCT_DIR/int2/bin/
+cd $PRODUCT_DIR/bin/
 unzip -o skysail.todos.jar
 chmod 755 todos_int2
 ./todos_int2 start

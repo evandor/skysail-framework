01b0e0ca74941243d8c4454e9ed6dbc94d0ebf28
Carsten Gräf <evandor@gmail.com>
2015-10-09 08:50:34 +0200

    wip

--- skysail.server.app.um.db/src/io/skysail/server/app/um/db/users/resources/PostUserResource.java
+++ skysail.server.app.um.db/src/io/skysail/server/app/um/db/users/resources/PostUserResource.java
@@ -1,13 +1,37 @@
 package io.skysail.server.app.um.db.users.resources;
 
-import io.skysail.server.app.um.db.domain.User;
+import io.skysail.api.responses.SkysailResponse;
+import io.skysail.server.app.um.db.UmApplication;
+import io.skysail.server.app.um.db.domain.*;
 import io.skysail.server.restlet.resources.PostEntityServerResource;
 
+import org.restlet.resource.ResourceException;
+
 public class PostUserResource extends PostEntityServerResource<User> {
+
+    private UmApplication app;
+
+    @Override
+    protected void doInit() throws ResourceException {
+        super.doInit();
+        app = (UmApplication)getApplication();
+    }
 
     @Override
     public User createEntityTemplate() {
         return new User();
+    }
+
+    @Override
+    public SkysailResponse<User> addEntity(User entity) {
+        String roles = getQueryValue("roles");
+
+        Role role = app.getRoleRepo().getById(roles);
+        entity.getRoles().add(role);
+
+        String id = app.getRepository().save(entity).toString();
+        entity.setId(id);
+        return new SkysailResponse<>(entity);
     }
 
     @Override

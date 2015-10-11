8cf05524918eacfa4b7863c461aed490850f50da
Carsten Gräf <evandor@gmail.com>
2015-10-11 22:32:12 +0200

    cleanup

--- skysail.api/src/io/skysail/api/links/LinkRole.java
+++ skysail.api/src/io/skysail/api/links/LinkRole.java
@@ -1,7 +1,7 @@
 package io.skysail.api.links;
 
 public enum LinkRole {
-    DEFAULT, APPLICATION_NAVIGATION, DOCUMENTATION, 
+    DEFAULT, APPLICATION_NAVIGATION,
     LIST_VIEW, // a link to be shown in the list entities table
     MENU_ITEM  // should be shown in the menu
 }

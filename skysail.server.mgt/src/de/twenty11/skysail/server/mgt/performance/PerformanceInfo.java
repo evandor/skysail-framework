e0142b6efbe34ac75944adf5929f7d4745d3a637
Carsten Gräf <evandor@gmail.com>
2015-11-22 16:50:25 +0100

    from different branch

---
+++ skysail.server.mgt/src/de/twenty11/skysail/server/mgt/performance/PerformanceInfo.java
@@ -0,0 +1,21 @@
+package de.twenty11.skysail.server.mgt.performance;
+
+import io.skysail.api.domain.Identifiable;
+import io.skysail.api.forms.Field;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class PerformanceInfo implements Identifiable{
+
+    private String id;
+
+    @Field
+    private String info;
+
+    public PerformanceInfo(String info) {
+        this.info = info;
+    }
+}

b0cc8eb306d341820757820551f51d93b01ad851
evandor <evandor@gmail.com>
2015-11-25 14:09:02 +0100

    designer fixes

--- skysail.server.designer.presentation/src/io/skysail/server/designer/presentation/InteractivePresentationApplication.java
+++ skysail.server.designer.presentation/src/io/skysail/server/designer/presentation/InteractivePresentationApplication.java
@@ -1,8 +1,11 @@
 package io.skysail.server.designer.presentation;
 
 import io.skysail.api.repos.*;
-import io.skysail.server.app.SkysailApplication;
+import io.skysail.server.app.*;
 import io.skysail.server.menus.MenuItemProvider;
+
+import java.util.Arrays;
+
 import aQute.bnd.annotation.component.*;
 import de.twenty11.skysail.server.app.ApplicationProvider;
 import de.twenty11.skysail.server.core.restlet.ApplicationContextId;
@@ -14,16 +17,16 @@ public class InteractivePresentationApplication extends SkysailApplication imple
     public static final String TODO_ID = "id";
     public static final String APP_NAME = "InteractivePresentation";
 
-    private InteractivePresentationRepository repo;
+    private TopicRepo repo;
 
     public InteractivePresentationApplication() {
-        super(APP_NAME);
+        super(APP_NAME, new ApiVersion(1), Arrays.asList(Topic.class));
         addToAppContext(ApplicationContextId.IMG, "/static/img/silk/page_link.png");
     }
 
-    @Reference(dynamic = true, multiple = false, optional = false, target = "(name=InteractivePresentationRepository)")
+    @Reference(dynamic = true, multiple = false, optional = false, target = "(name=TopicRepository)")
     public void setRepository(DbRepository repo) {
-        this.repo = (InteractivePresentationRepository) repo;
+        this.repo = (TopicRepo) repo;
     }
 
     public void unsetRepository(DbRepository repo) {

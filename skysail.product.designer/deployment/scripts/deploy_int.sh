ceda4501a5a8f342c18deb191310ba3de6c7ecf0
Carsten Gräf <evandor@gmail.com>
2016-01-05 06:47:17 +0100

    build script fix

--- skysail.product.designer/deployment/scripts/deploy_int.sh
+++ skysail.product.designer/deployment/scripts/deploy_int.sh
@@ -5,46 +5,46 @@
 APPNAME="designer"
 export JAVA_HOME=/home/carsten/.hudson/tools/hudson.model.JDK/java_SDK_8u25/
 
-cd /home/carsten/.hudson/jobs/ssp.$APPNAME.export.int/workspace/skysail.product.$APPNAME/generated/distributions/executable
+cd /home/carsten/.hudson/jobs/ssp.designer.export.int/workspace/skysail.product.designer/generated/distributions/executable
 echo "Creating ZIP Archive"
-cp $APPNAME.int.jar skysail.$APPNAME.jar
+cp designer.int.jar skysail.designer.jar
 
-zip -r skysail.$APPNAME.zip ../../../config/int skysail.$APPNAME.jar
-#zip -r skysail.$APPNAME.zip skysail.$APPNAME.jar
+zip -r skysail.designer.zip ../../../config/int skysail.designer.jar
+#zip -r skysail.designer.zip skysail.designer.jar
 
-echo "copying skysail.$APPNAME.zip to public site"
-cp skysail.$APPNAME.zip /var/www/skysail/products/$APPNAME/skysail.$APPNAME.int.zip
-cp skysail.$APPNAME.jar /var/www/skysail/products/$APPNAME/skysail.$APPNAME.int.jar
+echo "copying skysail.designer.zip to public site"
+cp skysail.designer.zip /var/www/skysail/products/designer/skysail.designer.int.zip
+cp skysail.designer.jar /var/www/skysail/products/designer/skysail.designer.int.jar
 
-mkdir -p /home/carsten/skysail/products/$APPNAME/int/bin
-mkdir -p /home/carsten/skysail/products/$APPNAME/int/lib
+mkdir -p /home/carsten/skysail/products/designer/int/bin
+mkdir -p /home/carsten/skysail/products/designer/int/lib
 
-echo "copying skysail.$APPNAME.jar to products directory"
-cp skysail.$APPNAME.jar /home/carsten/skysail/products/$APPNAME/int/bin/skysail.$APPNAME.jar
+echo "copying skysail.designer.jar to products directory"
+cp skysail.designer.jar /home/carsten/skysail/products/designer/int/bin/skysail.designer.jar
 
-echo "stopping $APPNAME service"
-/home/carsten/skysail/products/$APPNAME/int/bin/$APPNAME_int stop
+echo "stopping designer service"
+/home/carsten/skysail/products/designer/int/bin/designer_int stop
 
-cd /home/carsten/.hudson/jobs/ssp.$APPNAME.export.int/workspace/skysail.product.$APPNAME
-cp -r deployment/service/* /home/carsten/skysail/products/$APPNAME/int
+cd /home/carsten/.hudson/jobs/ssp.designer.export.int/workspace/skysail.product.designer
+cp -r deployment/service/* /home/carsten/skysail/products/designer/int
 
-# needed for $APPNAME functionality (to get access to the contained jars for compiling)
-#unzip -o /home/carsten/skysail/products/$APPNAME/int/bin/skysail.$APPNAME.jar
+# needed for designer functionality (to get access to the contained jars for compiling)
+#unzip -o /home/carsten/skysail/products/designer/int/bin/skysail.designer.jar
 
 #echo "getting config files for installation from svn"
-cd /home/carsten/skysail/products/$APPNAME/int/bin
+cd /home/carsten/skysail/products/designer/int/bin
 rm -rf config
 mkdir config
 cd config
-svn checkout https://85.25.22.125/repos/skysale/skysailconfigs/$APPNAME/int/
+svn checkout https://85.25.22.125/repos/skysale/skysailconfigs/designer/int/
 
-#echo "starting $APPNAME service"
-cd /home/carsten/skysail/products/$APPNAME/int/bin/
+#echo "starting designer service"
+cd /home/carsten/skysail/products/designer/int/bin/
 #rm -rf META-INF
 #rm -rf aQute
 #rm -rf jar
 # not really necessary:
-unzip -o skysail.$APPNAME.jar
+unzip -o skysail.designer.jar
 chmod 755 designer_int
 ./designer_int start
 

9358af49a233473863a9f41b45d0c7f69969a797
evandor <evandor@gmail.com>
2016-01-15 17:14:22 +0100

    uikit and webresource capability

---
+++ skysail.server.uikit.webresource/src/io/skysail/server/uikit/webresource/UiKitConstants.java
@@ -0,0 +1,6 @@
+package io.skysail.server.uikit.webresource;
+
+public interface UiKitConstants {
+    String BOOTSTRAP_WEB_RESOURCE_NAME = "/yootheme/uikit";
+    String BOOTSTRAP_WEB_RESOURCE_VERSION = "2.24.3";
+}

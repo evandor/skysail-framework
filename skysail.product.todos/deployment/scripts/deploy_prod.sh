76512cffdf90811ca7fd252a3ffeccc46cc81a9b
Carsten Gräf <evandor@gmail.com>
2016-02-07 18:07:27 +0100

    preparing todos prod deployment

--- skysail.product.todos/deployment/scripts/deploy_prod.sh
+++ skysail.product.todos/deployment/scripts/deploy_prod.sh
@@ -1,51 +1,20 @@
 #!/bin/bash -e
 
-export JAVA_HOME=/home/carsten/.hudson/tools/hudson.model.JDK/java_SDK_8u25/
+##########################################################################
+### Deployment Script SSP Todos Integration ##############################
+##########################################################################
 
-cd /home/carsten/.hudson/jobs/skysail.product.todos.export.prod/workspace/skysail.product.todos/generated/distributions/executable
+### CONFIGURATION ########################################################
+echo ""
+echo "Script Configuration:"
+echo "---------------------"
 
-#echo "Creating ZIP Archive"
-cp prod.jar skysail.todos.jar
-#zip -r skysail.todos.zip ../../../config/prod skysail.todos.jar
-zip -r skysail.todos.zip skysail.todos.jar
+APPNAME="todos"
+STAGE="prod"
 
-echo "copying skysail.todos.zip to public site"
-cp skysail.todos.zip /var/www/skysail/products/todos/skysail.todos.zip
+MAIN_DEPLOY_SCRIPT=../../../skysail.server/deployment/scripts/deploy.sh
+echo "calling $MAIN_DEPLOY_SCRIPT"
 
-mkdir -p /home/carsten/skysail/products/todos/prod/bin
-mkdir -p /home/carsten/skysail/products/todos/prod/lib
-
-echo "copying skysail.todos.jar to products directory"
-cp skysail.todos.jar /home/carsten/skysail/products/todos/prod/bin/skysail.todos.jar
-
-echo "stopping todos service"
-/home/carsten/skysail/products/todos/prod/bin/todos_prod stop
-
-cd /home/carsten/.hudson/jobs/skysail.product.todos.export.prod/workspace/skysail.product.todos
-cp -r deployment/service/* /home/carsten/skysail/products/todos/prod
-
-# needed for todos functionality (to get access to the contained jars for compiling)
-#unzip -o /home/carsten/skysail/products/todos/prod/bin/skysail.todos.jar
-
-#echo "getting config files for installation from svn"
-cd /home/carsten/skysail/products/todos/prod/bin
-rm -rf config
-mkdir config
-cd config
-svn checkout https://85.25.22.125/repos/skysale/skysailconfigs/todos/prod/
-
-#echo "starting todos service"
-cd /home/carsten/skysail/products/todos/prod/bin/
-#rm -rf META-INF
-#rm -rf aQute
-#rm -rf jar
-# not really necessary:
-unzip -o skysail.todos.jar
-chmod 755 todos_prod
-./todos_prod start
-
-
-
-
-
+chmod 775 $MAIN_DEPLOY_SCRIPT
+source $MAIN_DEPLOY_SCRIPT
 

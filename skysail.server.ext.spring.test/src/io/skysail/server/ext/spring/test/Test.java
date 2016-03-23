9e9235ea9022bb50b4655ab0ba81fad96ec8cd58
Carsten Gräf <evandor@gmail.com>
2016-03-23 08:56:44 +0100

    wip

--- skysail.server.ext.spring.test/src/io/skysail/server/ext/spring/test/Test.java
+++ skysail.server.ext.spring.test/src/io/skysail/server/ext/spring/test/Test.java
@@ -1,17 +1,19 @@
 package io.skysail.server.ext.spring.test;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
     public Test() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // instantiate our spring dao object from the application context
-        FileEventDao fileEventDao = (FileEventDao) ctx.getBean("fileEventDao");
-
-        // create a FileEventType object from the application context
-        FileEventType fileEventType = (FileEventType) ctx.getBean("fileEventType");
-
-        // insert the file event with the spring dao
-        fileEventDao.doInsert(fileEventType);
+//        FileEventDao fileEventDao = (FileEventDao) ctx.getBean("fileEventDao");
+//
+//        // create a FileEventType object from the application context
+//        FileEventType fileEventType = (FileEventType) ctx.getBean("fileEventType");
+//
+//        // insert the file event with the spring dao
+//        fileEventDao.doInsert(fileEventType);
     }
 }
